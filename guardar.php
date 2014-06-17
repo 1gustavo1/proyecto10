23e154fb951aa9f96a074c6f5f59e7e6b1f26013
1gustavo1 <ab1d@hotmail.es>
2014-06-16 22:30:05 -0500

    Update guardar.php

--- guardar.php
+++ guardar.php
@@ -77,12 +77,17 @@ $postString = '{ "key": "'.$api.'",
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $uri);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-$result = curl_exec($ch);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec_follow($ch);
+//curl_close($ch)
+
+//$result = curl_exec($ch);
 //echo "<br>".$result."<br>";
 
 
@@ -90,7 +95,7 @@ echo"<h1><div style='border: 10px solid black'>EXITO</div></h1>";
 echo "<head><script type='text/javascript'>
 function recarga(){
 location.href = 'Datos.php';
-/*En cambio esta nos llevaría a una dentro de nuestro dominio*/
+/*En cambio esta nos llevarÃ­a a una dentro de nuestro dominio*/
 location.href='Datos.php';
 }
 </script>
@@ -159,11 +164,91 @@ Stripe_Charge::create(array(
   "currency" => "usd",
   "customer" => $customer->id)
 );
-saveStripeCustomerId($user, $customer->id);
+//saveStripeCustomerId($user, $customer->id);
 } catch(Stripe_CardError $e) {
 echo "Error";
 }
 
+
+
+
+
+
+
+
+
+
+//----------------------
+function curl_exec_follow($ch, &$maxredirect = null) {
+  
+  // we emulate a browser here since some websites detect
+  // us as a bot and don't let us do our job
+  $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5)".
+                " Gecko/20041107 Firefox/1.0";
+  curl_setopt($ch, CURLOPT_USERAGENT, $user_agent );
+
+  $mr = $maxredirect === null ? 5 : intval($maxredirect);
+
+  if (ini_get('open_basedir') == '' && ini_get('safe_mode') == 'Off') {
+
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+  } else {
+    
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+    if ($mr > 0)
+    {
+      $original_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+      $newurl = $original_url;
+      
+      $rch = curl_copy_handle($ch);
+      
+      curl_setopt($rch, CURLOPT_HEADER, true);
+      curl_setopt($rch, CURLOPT_NOBODY, true);
+      curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+      do
+      {
+        curl_setopt($rch, CURLOPT_URL, $newurl);
+        $header = curl_exec($rch);
+        if (curl_errno($rch)) {
+          $code = 0;
+        } else {
+          $code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+          if ($code == 301 || $code == 302) {
+            preg_match('/Location:(.*?)\n/', $header, $matches);
+            $newurl = trim(array_pop($matches));
+            
+            // if no scheme is present then the new url is a
+            // relative path and thus needs some extra care
+            if(!preg_match("/^https?:/i", $newurl)){
+              $newurl = $original_url . $newurl;
+            }   
+          } else {
+            $code = 0;
+          }
+        }
+      } while ($code && --$mr);
+      
+      curl_close($rch);
+      
+      if (!$mr)
+      {
+        if ($maxredirect === null)
+        trigger_error('Too many redirects.', E_USER_WARNING);
+        else
+        $maxredirect = 0;
+        
+        return false;
+      }
+      curl_setopt($ch, CURLOPT_URL, $newurl);
+    }
+  }
+  return curl_exec($ch);
+}
 
 
 ?>
